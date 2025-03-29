d3b9c4fc0325bc91fac236db8ef091372b8b52cf
Ssekajja Wavamuno Isaac <ssekajjawavamuno@gmail.com>
2025-03-29 23:44:20 +0300

    Refactor multiply method, add third test

--- test/test.js
+++ test/test.js
@@ -1,14 +1,20 @@
 import { assert } from "chai";
 import { multiply } from "../multiply.js";
 
-describe("Testing multiplication", () => {
+describe("Testing multiplication on the first cycle", () => {
   it("Should multiply two values", () => {
     assert.equal(multiply(2, 1), 2);
   });
 });
 
-describe("Testing multiplication second cycle", () => {
+describe("Testing multiplication on a second cycle", () => {
   it("Should multiply two values", () => {
     assert.equal(multiply(2, 2), 4);
+  });
+});
+
+describe("Testing multiplication on a third cycle", () => {
+  it("Should multiply two values", () => {
+    assert.equal(multiply(3, 3), 9);
   });
 });
